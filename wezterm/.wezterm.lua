36be7bebe76b3061f6b3b745afe1a38082bcbb14
Christopher Jensen <cjensen5011@gmail.com>
2024-07-23 16:30:16 -0500

    spelling error

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -13,6 +13,6 @@ config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
 config.window_background_opacity = 0.8
-config.macos_window_background_blue = 10
+config.macos_window_background_blur = 10
 
 return config
