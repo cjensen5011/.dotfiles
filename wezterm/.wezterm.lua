833a9206f84c1e4b29e8ea74ebaad9f5713678d7
Christopher Jensen <cjensen5011@gmail.com>
2024-07-23 22:44:57 -0500

    fixed theme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 -- configs
 
-config.color_scheme = "catppuccin-mocha"
+config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 16
