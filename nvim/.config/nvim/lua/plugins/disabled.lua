71b7e584b7b6ce25f2f3a87029e8b7841679f24b
jensen ಠ_ಠ <cjensen5011@gmail.com>
2025-02-06 15:08:58 -0600

    i tried

---
+++ nvim/.config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,6 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false }, -- disable fancy tabs
+  { "folke/noice.nvim", enabled = false }, -- disable the popup stuff
+  { "echasnovski/mini.indentscope", enabled = false }, -- disable indent lines
+  { "lukas-reineke/indent-blankline.nvim", enabled = false }, -- disable indent lines
+}
